a4edb3b1010c4616d967976e36f14f883ba4fe44
diego rodas garcia <diegorodas2008@hotmail.com>
2021-09-16 13:00:35 -0500

    Webhook post.

--- src/app.js
+++ src/app.js
@@ -72,6 +72,10 @@ app.get('/webhook', (req, res) => {
     }
 })
 
+app.post('/webhook', (req, res) => {
+    console.log('json', req)
+})
+
 // server.listen(port, function () {
 //     console.log('App running on *: ' + port);
 // });
